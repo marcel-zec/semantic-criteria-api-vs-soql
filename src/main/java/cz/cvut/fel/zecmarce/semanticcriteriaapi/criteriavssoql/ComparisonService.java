2cb635c30d1849b1ed9288fbc9bfe4ca028498c4
Marcel Žec <zecmarce@fel.cvut.cz>
2021-05-13 13:07:07 +0200

    more comparisons, change results showing

--- src/main/java/cz/cvut/fel/zecmarce/semanticcriteriaapi/criteriavssoql/ComparisonService.java
+++ src/main/java/cz/cvut/fel/zecmarce/semanticcriteriaapi/criteriavssoql/ComparisonService.java
@@ -9,7 +9,6 @@ import cz.cvut.kbss.jopa.model.query.criteria.ParameterExpression;
 import cz.cvut.kbss.jopa.model.query.criteria.Predicate;
 import cz.cvut.kbss.jopa.model.query.criteria.Root;
 import cz.cvut.kbss.jopa.sessions.CriteriaBuilder;
-import org.eclipse.rdf4j.query.algebra.In;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +17,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,6 +26,8 @@ public class ComparisonService implements ApplicationListener<ApplicationReadyEv
 
     private final EntityManager em;
     private final GameRepository gr;
+    private int failed = 0;
+    private int amount = 0;
 
     @Autowired
     public ComparisonService(EntityManager em, GameRepository gr) {
@@ -36,14 +38,33 @@ public class ComparisonService implements ApplicationListener<ApplicationReadyEv
     @Transactional
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        comparison1();
-        comparison2();
-        comparison3();
-//        comparison4();
-        comparison5();
+        amount++;
+        if (!comparison1()) failed++;
+
+        amount++;
+        if (!comparison2()) failed++;
+
+        amount++;
+        if (!comparison3()) failed++;
+
+        amount++;
+        if (!comparison4()) failed++;
+
+        amount++;
+        if (!comparison5()) failed++;
+
+        amount++;
+        if (!comparison6()) failed++;
+
+        amount++;
+        if (!comparison7()) failed++;
+
+        System.out.println("\nAMOUNT OF COMPARISON: " + amount);
+        System.out.println("FAILED: " + failed);
+        System.out.println("PASSED: " + (amount - failed) + "\n");
     }
 
-    private void comparison1(){
+    private boolean comparison1(){
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Game> query = cb.createQuery(Game.class);
         Root<Game> game = query.from(Game.class);
@@ -53,10 +74,14 @@ public class ComparisonService implements ApplicationListener<ApplicationReadyEv
 
         final List<Game> soqlResults = em.createQuery("SELECT g FROM Game g", Game.class).getResultList();
 
-        compare(criteriaResults,soqlResults, "simply all");
+        boolean result = compare(criteriaResults,soqlResults, "simply all");
+        System.out.println("Criteria result: " + criteriaResults);
+        System.out.println("SOQL result:     " + soqlResults);
+        System.out.println("----------------------------------------------");
+        return result;
     }
 
-    private void comparison2(){
+    private boolean comparison2(){
         final String parameterValue = "Stronghold 3";
 
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -69,10 +94,54 @@ public class ComparisonService implements ApplicationListener<ApplicationReadyEv
 
         final Game soqlResults = em.createQuery("SELECT g FROM Game g WHERE g.name = :name", Game.class).setParameter("name", parameterValue, "en").getSingleResult();
 
-        compare(criteriaResults,soqlResults, "whereNameEqual");
+        boolean result = compare(criteriaResults,soqlResults, "simple where");
+        System.out.println("Criteria result: " + criteriaResults);
+        System.out.println("SOQL result:     " + soqlResults);
+        System.out.println("----------------------------------------------");
+        return result;
     }
 
-    private void comparison3(){
+    private boolean comparison3(){
+        final String parameterValue = "Stronghold 3";
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Game> query = cb.createQuery(Game.class);
+        Root<Game> game = query.from(Game.class);
+        ParameterExpression<String> name = cb.parameter(String.class);
+        query.select(game).where(cb.not(cb.equal(game.getAttr(Game_.name), name)));
+        TypedQuery<Game> typedQuery = em.createQuery(query).setParameter(name,parameterValue,"en");
+        final List<Game> criteriaResults = typedQuery.getResultList();
+
+        final List<Game> soqlResults = em.createQuery("SELECT g FROM Game g WHERE NOT g.name = :name", Game.class).setParameter("name", parameterValue, "en").getResultList();
+
+        boolean result = compare(criteriaResults,soqlResults, "where equal negated");
+        System.out.println("Criteria result: " + criteriaResults);
+        System.out.println("SOQL result:     " + soqlResults);
+        System.out.println("----------------------------------------------");
+        return result;
+    }
+
+    private boolean comparison4(){
+        final Integer amountValue = 15;
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Game> query = cb.createQuery(Game.class);
+        Root<Game> game = query.from(Game.class);
+        ParameterExpression<Integer> amount = cb.parameter(Integer.class);
+        query.select(game).where(cb.lessThanOrEqual(game.getAttr(Game_.developer).getAttr(Developer_.employeeCount), amount));
+        TypedQuery<Game> typedQuery = em.createQuery(query).setParameter(amount,amountValue);
+        final List<Game> criteriaResults = typedQuery.getResultList();
+
+        final List<Game> soqlResults = em.createQuery("SELECT g FROM Game g WHERE g.developer.employeeCount <= :amount", Game.class).setParameter("amount", amountValue).getResultList();
+
+        boolean result = compare(criteriaResults,soqlResults, "path joins + <");
+        System.out.println("Criteria result: " + criteriaResults);
+        System.out.println("SOQL result:     " + soqlResults);
+        System.out.println("----------------------------------------------");
+        return result;
+    }
+
+    private boolean comparison5(){
         final Integer parameterValue = 50;
 
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -85,11 +154,15 @@ public class ComparisonService implements ApplicationListener<ApplicationReadyEv
 
         final Integer soqlResults = em.createQuery("SELECT COUNT(d) FROM Developer d WHERE d.employeeCount >= :amount", Integer.class).setParameter("amount", parameterValue).getSingleResult();
 
-        compare(criteriaResults,soqlResults, "countWhereEmployeeNumberEqual");
+        boolean result = compare(criteriaResults,soqlResults, "count + >=");
+        System.out.println("Criteria result: " + criteriaResults);
+        System.out.println("SOQL result:     " + soqlResults);
+        System.out.println("----------------------------------------------");
+        return result;
     }
 
-    private void comparison4(){
-        final String nameParameterValue = "%Studio%";
+    private boolean comparison6(){
+        final String nameParameterValue = "Studio";
         final Integer amountParameterValue = 500;
 
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -100,16 +173,20 @@ public class ComparisonService implements ApplicationListener<ApplicationReadyEv
         Predicate nameRestriction = cb.like(developer.getAttr(Developer_.name), name);
         Predicate amountRestriction = cb.lessThan(developer.getAttr(Developer_.employeeCount),amount);
         query.select(developer).where(nameRestriction,amountRestriction);
-        TypedQuery<Developer> typedQuery = em.createQuery(query).setParameter(name, nameParameterValue,"en").setParameter(amount, amountParameterValue);
+        TypedQuery<Developer> typedQuery = em.createQuery(query).setParameter(name, nameParameterValue).setParameter(amount, amountParameterValue);
         final List<Developer> criteriaResults = typedQuery.getResultList();
 
-        final List<Developer> soqlResults = em.createQuery("SELECT d FROM Developer d WHERE d.employeeCount < :amount AND d.name = :name", Developer.class)
-                .setParameter("amount", amountParameterValue).setParameter("name",nameParameterValue,"en").getResultList();
+        final List<Developer> soqlResults = em.createQuery("SELECT d FROM Developer d WHERE d.employeeCount < :amount AND d.name LIKE :name", Developer.class)
+                .setParameter("amount", amountParameterValue).setParameter("name",nameParameterValue).getResultList();
 
-        compare(criteriaResults,soqlResults, "likeAnd");
+        boolean result = compare(criteriaResults,soqlResults, "like + and");
+        System.out.println("Criteria result: " + criteriaResults);
+        System.out.println("SOQL result:     " + soqlResults);
+        System.out.println("----------------------------------------------");
+        return result;
     }
 
-    private void comparison5(){
+    private boolean comparison7(){
         final Integer value1 = 1000;
         final Integer value2 = 10;
 
@@ -130,67 +207,68 @@ public class ComparisonService implements ApplicationListener<ApplicationReadyEv
         final List<Developer> soqlResults = em.createQuery("SELECT d FROM Developer d WHERE d.employeeCount >= :param1 OR d.employeeCount <= :param2 ORDER BY d.employeeCount DESC, d.name ASC" , Developer.class)
                 .setParameter("param1", value1).setParameter("param2",value2).getResultList();
 
-        compareWithOrder(criteriaResults,soqlResults, "orOrder");
+        boolean result = compareWithOrder(criteriaResults,soqlResults, "or + order");
+        System.out.println("Criteria result: " + criteriaResults);
+        System.out.println("SOQL result:     " + soqlResults);
+        System.out.println("----------------------------------------------");
+        return result;
     }
 
-    private void compare(List<? extends IdentifiableEntity> x, List<? extends IdentifiableEntity> y, String methodName){
-        System.out.println("\n-----* " + methodName + " *-----");
-        System.out.println(x);
-        System.out.println(y);
+    private boolean compare(List<? extends IdentifiableEntity> x, List<? extends IdentifiableEntity> y, String methodName){
+        System.out.println("\n----- " + methodName + " -----");
         if (x.size() == y.size()){
             for (IdentifiableEntity e: x) {
                 if (!y.stream().anyMatch(item -> item.getUri().equals(e.getUri()))){
-                    System.out.println("--- DIFFERENT RESULTS");
-                    return;
+                    System.out.println("--- DIFFERENT RESULTS -----------------------");
+                    return false;
                 }
             }
-            System.out.println("--- SAME RESULTS");
+            System.out.println("--- SAME RESULTS ----------------------------");
+            return true;
         } else {
-            System.out.println("--- DIFFERENT RESULTS");
+            System.out.println("--- DIFFERENT RESULTS -----------------------");
+            return false;
         }
-        System.out.println("----------------------------------------\n");
     }
 
-    private void compareWithOrder(List<? extends IdentifiableEntity> x, List<? extends IdentifiableEntity> y, String methodName){
-        System.out.println("\n-----* " + methodName + " *-----");
-        System.out.println(x);
-        System.out.println(y);
+    private boolean compareWithOrder(List<? extends IdentifiableEntity> x, List<? extends IdentifiableEntity> y, String methodName){
+        System.out.println("\n----- " + methodName + " -----");
         if (x.size() == y.size()){
             for (int i = 0; i < x.size(); i++) {
                 if (!x.get(i).getUri().equals(y.get(i).getUri())){
-                    System.out.println("--- DIFFERENT RESULTS");
-                    return;
+                    System.out.println("--- DIFFERENT RESULTS -----------------------");
+                    return false;
                 }
             }
-            System.out.println("--- SAME RESULTS");
+            System.out.println("--- SAME RESULTS ----------------------------");
+            return true;
         } else {
-            System.out.println("--- DIFFERENT RESULTS");
+            System.out.println("--- DIFFERENT RESULTS -----------------------");
+            return false;
         }
-        System.out.println("----------------------------------------\n");
     }
 
-    private void compare(IdentifiableEntity x, IdentifiableEntity y, String methodName){
-        System.out.println("\n-----* " + methodName + " *-----");
-        System.out.println(x);
-        System.out.println(y);
+    private boolean compare(IdentifiableEntity x, IdentifiableEntity y, String methodName){
+        System.out.println("\n----- " + methodName + " -----");
         if (x.getUri().equals(y.getUri())){
-            System.out.println("--- SAME RESULTS");
+            System.out.println("--- SAME RESULTS ----------------------------");
+            return true;
         } else {
-            System.out.println("--- DIFFERENT RESULTS");
+            System.out.println("--- DIFFERENT RESULTS -----------------------");
+            return false;
         }
-        System.out.println("----------------------------------------\n");
+
     }
 
-    private void compare(Integer x, Integer y, String methodName){
-        System.out.println("\n-----* " + methodName + " *-----");
-        System.out.println(x);
-        System.out.println(y);
+    private boolean compare(Integer x, Integer y, String methodName){
+        System.out.println("\n----- " + methodName + " -----");
         if (x.equals(y)){
-            System.out.println("--- SAME RESULTS");
+            System.out.println("--- SAME RESULTS ----------------------------");
+            return true;
         } else {
-            System.out.println("--- DIFFERENT RESULTS");
+            System.out.println("--- DIFFERENT RESULTS -----------------------");
+            return false;
         }
-        System.out.println("----------------------------------------\n");
     }
 
 
